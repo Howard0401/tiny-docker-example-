a6051edb8652e1755f799b35764b1f1369aacc5e
howard chen <chenhoward0401@gmail.com>
2021-04-16 12:13:12 +0800

    feat: [Howard] deploy.sh -> fix syntex error, edit dockerfile

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	i := 0 // range of int32: from -2,147,483,648 to 2,147,483,647
 	for {
+		i += 1
 		i64, err := safecast.Int32(i)
 		if err != nil {
 			if strings.Contains(err.Error(), "int32 out of range") {
