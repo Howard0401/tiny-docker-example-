757675f714233eabfda7caa279b0aca7c52fe6a1
howard chen <chenhoward0401@gmail.com>
2021-04-16 11:52:19 +0800

    feat: [Howard] modify deploy.sh for refactor

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,24 @@
-export REMOVE_IMAGE=$(docker images -q tiny_test_log)
+export IMG_NAME=tiny_test_log
+export ACTIVE_IMAGE_ID=$(docker inspect --format="{{.Id}}" $IMG_NAME)
+export REMOVE_IMAGE=$(docker images -q $IMG_NAME)
 
-if [ "$REMOVE_IMAGE" != "" ]; then
-    docker rm -f $REMOVE_IMAGE
-fi
+# RMI for remove active container, image
+function RMI() {
+    if [ "$1" != ""]; then
+        docker rmi -f $1
+    fi
+    if [ "$2" != "" ]; then
+        docker rmi -f $2
+    fi
+}
+
+RMI($ACTIVE_IMAGE_ID, $REMOVE_IMAGE)
+
+
+
+    # if [ "$ACTIVE_IMAGE_ID" != ""]; then
+    #     docker rmi -f $ACTIVE_IMAGE_ID
+    # fi
+    # if [ "$REMOVE_IMAGE" != "" ]; then
+    # docker rmi -f $REMOVE_IMAGE
+    # fi
