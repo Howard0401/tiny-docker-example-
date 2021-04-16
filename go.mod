db9d89d6dff437b3e043ba726d3ff627c4499a11
howard chen <chenhoward0401@gmail.com>
2021-04-16 10:46:01 +0800

    feat: [Howard] add go-safecast

---
+++ go.mod
@@ -0,0 +1,5 @@
+module tiny-docker-example
+
+go 1.16
+
+require github.com/rung/go-safecast v1.0.1 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"strings"
+
+	"github.com/rung/go-safecast"
+)
 
 func main() {
-
+	i := 0 // range of int32: from -2,147,483,648 to 2,147,483,647
 	for {
-		fmt.Println("test for log size")
+		i64, err := safecast.Int32(i)
+		if err != nil {
+			if strings.Contains(err.Error(), "int32 out of range") {
+				i = 0
+			}
+		}
+		log.Println(i64)
 	}
+
 }
