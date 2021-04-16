31b0add458516a7c93e9b4a83d7b4ae9ec36cbb3
howard chen <chenhoward0401@gmail.com>
2021-04-16 12:27:00 +0800

    feat: [Howard] deploy.sh -> fix syntex error, edit dockerfile

--- main.go
+++ main.go
@@ -2,22 +2,20 @@ package main
 
 import (
 	"log"
-	"strings"
-
-	"github.com/rung/go-safecast"
 )
 
 func main() {
 	i := 0 // range of int32: from -2,147,483,648 to 2,147,483,647
 	for {
 		i += 1
-		i64, err := safecast.Int32(i)
-		if err != nil {
-			if strings.Contains(err.Error(), "int32 out of range") {
-				i = 0
-			}
-		}
-		log.Println(i64)
+		// i64, err := safecast.Int32(i)
+		// if err != nil {
+		// 	if strings.Contains(err.Error(), "int32 out of range") {
+		// 		i = 0
+		// 	}
+		// }
+		// log.Println(i64)
+		log.Println(i)
 	}
 
 }
